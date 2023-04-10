022012bacbe9e06d5ceda0c12f6d9551acaf8094
Artur Makaryuk <art.makaryuk@gmail.com>
2023-04-10 16:06:36 +0700

    work

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,32 @@
 package jm.task.core.jdbc;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+
+    private static final  String URL = "jdbc:mysql://localhost:3306/test";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        Connection connection;
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("Соединение установлено");
+            }
+            connection.close();
+            if (connection.isClosed()) {
+                System.out.println("Соединение закрыто");
+            }
+        } catch (SQLException e) {
+            System.out.println("Не удалось соединиться");
+        }
     }
 }
